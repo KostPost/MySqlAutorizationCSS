f312b8ea3cffd7e2761f40541beee21fde115912
KostPost <kt5082206@gmail.com>
2023-09-25 19:35:23 +0300

    Add project files.

---
+++ MySqlAutorizationCSS/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySqlAutorizationCSS
+{
+    public class Account
+    {
+        [Key]
+        public int user_id { get; set; }
+        public string user_name { get; set; }
+        public string user_password { get; set;}
+
+        //public Account(string user_name,string user_password)
+        //{
+        //    this.user_name = user_name;
+        //    this.user_password = user_password;
+        //}
+
+        public void Print()
+        {
+            Console.WriteLine($"\nid = {user_id} \nname = {user_name}\npassword = {user_password}\n");
+        }
+        
+        public void FindByName(String name)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                string nameToFind = "qwe";
+                var users = db.acc_authorization.Where(u => u.user_name == nameToFind).ToList();
+
+                Console.WriteLine($"Accounts with name - {nameToFind}:");
+
+                foreach (var acc in users)
+                {
+                    acc.Print();
+                }
+
+            }
+        }
+
+    }
+}
---
+++ MySqlAutorizationCSS/ApplicationContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace MySqlAutorizationCSS
+{
+    public class ApplicationContext : DbContext
+    {
+
+        public DbSet<Account> acc_authorization { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=localhost;user=root;password=kostpost;database=authorization;",
+                new MySqlServerVersion(new Version(8, 0, 11))
+            );
+        }
+    }
+}
